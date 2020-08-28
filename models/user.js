d2a461fe31e5a5faf71963230817de2b07d64045
joseph <josephjohndemarco@gmail.com>
2020-08-28 15:35:49 -0400

    Register app with OAuth.

--- models/user.js
+++ models/user.js
@@ -5,7 +5,6 @@ const Schema = mongoose.Schema;
 var userSchema = new mongoose.Schema({
     name: String,
     email: String,
-    cohort: String,
     avatar: String,
     workouts: [workoutSchema],
     googleId: String
