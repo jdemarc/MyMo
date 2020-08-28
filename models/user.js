60795064be43256809f9bb26200b2cb7402d267e
joseph <josephjohndemarco@gmail.com>
2020-08-28 17:42:15 -0400

    Successfully implement OAuth.

--- models/user.js
+++ models/user.js
@@ -2,12 +2,14 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-var userSchema = new mongoose.Schema({
+var userSchema = new Schema({
     name: String,
     email: String,
     avatar: String,
-    workouts: [workoutSchema],
+    //workouts: [workoutSchema],
     googleId: String
   }, {
     timestamps: true
   });
+
+  module.exports = mongoose.model('User', userSchema);
