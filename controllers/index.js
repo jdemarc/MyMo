b0c83a6ba381a0e53af6a6a4a8da6e5367f2bd0a
joseph <josephjohndemarco@gmail.com>
2020-08-31 23:34:33 -0400

    Complete rudimentary stats.

--- controllers/index.js
+++ controllers/index.js
@@ -12,15 +12,19 @@ function showStats(req, res) {
 
     Workout.aggregate([
         { $match: { user: ObjectId(req.user.id) } },
-        { $group: { _id: null, durationSum: { $sum: "$duration" }, calorieSum: { $sum: "$calories"} } },
+        { $group: { _id: null, durationSum: { $sum: "$duration" }, calorieSum: { $sum: "$calories"},
+            workoutSum: { $sum: 1 }} },
 
     ], function (err, stats) {
         if (err) console.log('Error');
 
+        console.log(stats);
+
         let totalDuration = convertTime(stats[0].durationSum);
         let totalCalories = stats[0].calorieSum;
+        let totalWorkouts = stats[0].workoutSum;
 
-        res.render('home', { title: 'MyMo', totalDuration, totalCalories });
+        res.render('home', { title: 'MyMo', totalDuration, totalCalories, totalWorkouts });
     });
 };
 
