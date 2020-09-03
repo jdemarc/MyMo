e10cb00b7f58733eabc41974e711d636afbcb4bf
joseph <josephjohndemarco@gmail.com>
2020-09-03 11:18:26 -0400

    Prevent users who did not create workout from adding and deleting exercises in that workout.

--- controllers/exercises.js
+++ controllers/exercises.js
@@ -7,21 +7,38 @@ module.exports = {
 }
 
 function create(req, res) {
-    Workout.findById(req.params.id, function(err, workout) {
-        workout.exercises.push(req.body);
-        workout.save(function(err) {
-            if (err) console.log("Error adding exercise.");
+    console.log(req.user);
 
+    Workout.findById(req.params.id, function(err, workout) {
+        console.log(workout);
+
+        // Prevent a user with a different ID from adding exercises to a workout they did not create.
+        if (req.user.id !== workout.user) {
             res.redirect(`/workouts/${workout._id}`);
-        });
+        } else {
+            workout.exercises.push(req.body);
+            workout.save(function(err) {
+                if (err) console.log("Error adding exercise.");
+    
+                res.redirect(`/workouts/${workout._id}`);
+            });
+        }
     });
 }
 
 function delExercise(req, res) {
     Workout.findOne({ "exercises._id" : req.params.id }, function(err, workout) {
-        workout.exercises.id(req.params.id).remove();
-        workout.save(function(err) {
-            res.redirect(`/workouts/${workout._id}`)
-        })
+
+        //Prevent user who did not create the workout from deleting exercises in a workout.
+        if (req.user.id !== workout.user) {
+            res.redirect(`/workouts/${workout._id}`);
+        } else {
+            workout.exercises.id(req.params.id).remove();
+            workout.save(function(err) {
+                if (err) console.log("Error deleting exercise.");
+            
+                res.redirect(`/workouts/${workout._id}`)
+            });
+        }
     });
 }
